ee663c663fa9164ea1f3ecee651bb00d3b1dfd1c
abhiruk <abhirukprashan436@gmail.com>
2026-04-29 17:22:25 +0530

    compress

--- main.go
+++ main.go
@@ -5,8 +5,26 @@ import (
     "os"
 )
 
-func compress(){
-   fmt.Println("Compressing")
+func compress(data []byte) []byte {
+    if len(data) == 0 {
+        return nil
+    }
+    var encoded []byte
+    count := 0
+    prev := data[0]
+
+    for i := 1; i < len(data); i++ {
+        if data[i] == prev && count < 255 {
+            count++
+        } else {
+            encoded = append(encoded, byte(count), prev)
+            prev = data[i]
+            count = 1
+        }
+    }
+    encoded = append(encoded, byte(count), prev)
+    return encoded
+}
 }
 func decompress(){
    fmt.Println("Decompressing")
@@ -18,18 +36,21 @@ func main() {
         fmt.Println("Usage: compressor <file>")
         os.Exit(1)
     }
+
     command := os.Args[1]
-/*
+    var results []byte
+
+
     file, err := os.ReadFile(os.Args[2])
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
 
-*/
     switch command {
     case "compress":
-        compress()
+       results = compress(file)
+       fmt.Println(results)
     case "decompress":
         decompress()
     default:
